ee8023d0a257b73c25aa1cdfe04c938cc8c2a9fe
zhanghh <2068325343@qq.com>
2024-09-22 11:33:44 +0800

    解决冲突

--- MystarterApplication.java
+++ MystarterApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MystarterApplication {
     
-    // 添加注释
     public static void main(String[] args) {
         SpringApplication.run(MystarterApplication.class, args);
     }
